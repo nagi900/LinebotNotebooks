3356f26fdbee296867681d5d0725ff72b6608757
nagi900 <kusanagi26358@gmail.com>
2021-01-03 11:49:35 +0900

    notebook/urls.pyを少し変更

--- notebooks/urls.py
+++ notebooks/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path ,include
 
 urlpatterns = [
-     path('callback/', include('bot.urls')),
+    path('callback/', include('bot.urls')),
 ]
