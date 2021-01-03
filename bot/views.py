89163091b75c81d8d14180cddfa04bf3d24a2781
nagi900 <kusanagi26358@gmail.com>
2021-01-03 20:37:12 +0900

    相槌を追加ただしpushメッセージではなくreply

--- bot/views.py
+++ bot/views.py
@@ -19,7 +19,8 @@ line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 #返事
-back_channneling = {"おっけ","りょ","うい","あい","おけい","まかせな"}
+back_channnelings = {"おっけ","りょ","うい","あい","おけい","まかせな"}
+back_channelings=back_channnelings(random.randamint(0,5))
 
 
 @csrf_exempt
@@ -37,7 +38,7 @@ def callback(request):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     line_bot_api.reply_message(event.reply_token,
-                                [TextSendMessage(text=event.message.text + "\n okよ"),TextMessage(text="二つ目の返事ok"),TextMessage(text=back_channneling(random.randamint(0,5)))])
+                                [TextSendMessage(text=event.message.text + "\n okよ"),TextMessage(text="二つ目の返事ok"),TextMessage(text=back_channelings)])
 #応答は一度しかできない 配列で返せば5つまでいける
 
-    line_bot_api.push_message(event.to,TextMessage(back_channneling(random.randamint(0,5))))
+    line_bot_api.push_message(event.to,TextMessage(back_channnelings(random.randamint(0,5))))
