c942fb99e2f71c3e2c384beb02efdb7c3469ec55
nagi900 <kusanagi26358@gmail.com>
2021-01-03 21:12:34 +0900

    関数の中で定義しないといけないのか検証

--- bot/views.py
+++ bot/views.py
@@ -37,9 +37,11 @@ def callback(request):
 # オウム返し
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
+    doko = "関数の中"
     line_bot_api.reply_message(event.reply_token,
                                 [TextSendMessage(text=event.message.text + "\n okよ"),
-                                TextMessage(text="二つ目の返事ok")])
+                                TextMessage(text="二つ目の返事ok"),
+                                TextMessage(text=doko)])
 #応答は一度しかできない 配列で返せば5つまでいける
 
 #    line_bot_api.push_message(event.to,TextMessage(back_channnelings(random.randamint(0,5))))
